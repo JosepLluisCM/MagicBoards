eb08a2ff7abce3dc44ce39d810f8b1e4c1c9b3bc
JosepLluisCM <webdevJosep@gmail.com>
2024-11-14 20:35:21 +0100

    add

---
+++ server/Controllers/FirestoreController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Google.Cloud.Firestore;
+using backend.Services;
+
+
+namespace backend.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class FirestoreController : ControllerBase
+  {
+    private readonly FirestoreService _firestoreService;
+    private readonly FirestoreDb _firestoreDb;
+
+    // FirestoreService is injected here
+    public FirestoreController(FirestoreService firestoreService)
+    {
+      _firestoreService = firestoreService;
+      _firestoreDb = _firestoreService.GetFirestoreDb();
+    }
+
+    [HttpGet("documents")]
+    public async Task<IActionResult> GetDocuments()
+    {
+      var collection = _firestoreDb.Collection("testCollection");
+      var snapshot = await collection.GetSnapshotAsync();
+      return Ok(snapshot.Documents.Select(doc => doc.Id));
+    }
+  }
+}
---
+++ server/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using Google.Cloud.Firestore;
+
+namespace backend.Models
+{
+    [FirestoreData]
+    public class Item
+    {
+        [FirestoreProperty]
+        public string Id { get; set; }
+        [FirestoreProperty]
+        public string Description { get; set; } = string.Empty;
+        [FirestoreProperty]
+        public bool Checked { get; set; } = false;
+    }
+}
